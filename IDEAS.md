09fbd82fb37751a1169ec39696d991793795220c
Daniel Oltmanns <daniel.oltmanns@outlook.de>
2017-09-22 21:09:01 +0200

    Begin from zero

---
+++ IDEAS.md
@@ -0,0 +1,26 @@
+# Ideas for Version 3.0
+
+## Needed
+- [ ] New Tooltips
+- [ ] Color Updates
+- [ ]  A new font (Roboto/Rubik/Roboto Mono/..)
+- [ ] Some minor fixes
+- [ ] New Components
+    - [ ] Pagination
+    - [ ] Progress bars
+    - [ ] Element groups
+    - [ ] Cards
+    - [ ] Input Groups
+    - [ ] Lists Groups
+    - [ ] Labels
+    - [ ] Button Groups
+    - [ ] Navs (Tabs/Pills)
+- [ ] Rename some classes
+- [ ] Finally roll out a complete new Version of Skeleton (Cause if the huge changes,  I think we need to move from v2 to v3)
+
+## Optional
+- [ ] Optional Flexbox-grid
+
+## Notes
+
+- Classes that need renaming:
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var banner = require('gulp-banner');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const banner = require('gulp-banner');
+const rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/skeleton-plus.scss')
+  return gulp.src('./scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
-    .pipe(banner('/* skeleton-plus <https://github.com/oldaniel/skeleton-plus> , Copyright 2016, Daniel Oltmanns <daniel@thedcdesigns.com> */\n', {}))
+    .pipe(banner('/* skeleton-plus <https://github.com/oldaniel/skeleton-plus> , Copyright 2017, Daniel Oltmanns <daniel@thedcdesigns.com> */\n', {}))
+    .pipe(rename('skeleton-plus.css'))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:min', function () {
-  return gulp.src('./scss/skeleton-plus.scss')
+  return gulp.src('./scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(banner('/* skeleton-plus <https://github.com/oldaniel/skeleton-plus> , Copyright 2016, Daniel Oltmanns <daniel@thedcdesigns.com> */\n', {}))
+    .pipe(banner('/* skeleton-plus <https://github.com/oldaniel/skeleton-plus> , Copyright 2017, Daniel Oltmanns <daniel@thedcdesigns.com> */\n', {}))
     .pipe(rename('skeleton-plus.min.css'))
     .pipe(gulp.dest('./css'));
 });
